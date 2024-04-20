17da8cd75fffca22a87ec24e41e2ed85fb1a973f
BaHwan <beingbook@gmail.com>
2024-04-20 23:56:54 +0900

    test

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -3,3 +3,27 @@
 export async function testAction() {
   return "test";
 }
+
+export async function testAction2() {
+  return "test";
+}
+
+export async function testAction3() {
+  return "test";
+}
+
+export async function testAction4() {
+  return "test";
+}
+
+export async function testAction5() {
+  return "test";
+}
+
+export async function testAction6() {
+  return "test";
+}
+
+export async function testAction7() {
+  return "test";
+}
