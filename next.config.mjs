2a6b9ecbb586ad4b759ecafcafb2419eca9440a5
BaHwan <beingbook@gmail.com>
2024-04-20 23:14:56 +0900

    Initial commit from Create Next App

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverSourceMaps: true,
+  },
+};
+
+export default nextConfig;
---
+++ src/app/(test)/test/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { testAction } from "../../actions";
+
+export default function Page() {
+  return (
+    <form
+      action={async () => {
+        console.log(await testAction());
+      }}
+    >
+      <button>submit</button>
+    </form>
+  );
+}
